35ae06301ab4c60992d7581f8fa28f8b7f089ed5
KaiWangdata <72056285+KaiWangdata@users.noreply.github.com>
2020-12-02 17:40:50 -0500

    added more terms for comparison

--- server.R
+++ server.R
@@ -124,26 +124,6 @@ function(input, output, session) {
     )
     output$table <- renderDataTable(vgsales)
     
-    output$mymap <- renderLeaflet({
-        v <- vgsales %>%
-            filter(Genre == input$Genre &
-                       Platform == input$Platform) %>%
-            select(Year, 'sales' = starts_with(input$Region, ignore.case = F)) %>%
-              summarise(n = sum(sales))
-        if (input$Region=='NA'){
-        leaflet() %>% addTiles(attribution = 'Number of games sold in NA') %>% 
-            addMarkers(lng=-74.0059, lat=40.7128, popup=paste(as.character(v$n),'million copies sold!'))
-        } else if (input$Region=='EU'){
-            leaflet() %>% addTiles(attribution = 'Number of games sold in EU') %>% 
-                addMarkers(lng=2.352222, lat=48.856613, popup=paste(as.character(v$n),'million copies sold!'))
-        } else if (input$Region=='JP'){
-            leaflet() %>% addTiles(attribution = 'Number of games sold in JP') %>% 
-                addMarkers(lng=141.237656, lat=40.592312, popup=paste(as.character(v$n),'million copies sold!'))
-        }else {
-            leaflet() %>% addTiles(attribution = 'Number of games sold in the World') %>% 
-                addMarkers(lng=-79.383186, lat=43.653225, popup=paste(as.character(v$n),'million copies sold!'))
-        }
-    })
-    
+   
 }
 
